b1a31b4a03e4b68a7664227db7769c4dc356bfd6
XRenso <tch.xr@yandex.ru>
2024-02-26 16:53:20 +1100

    Mega team

--- Web/Web/urls.py
+++ Web/Web/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from main import views as m_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',m_view.index)
+    path('',m_view.index),
+    path('team/',m_view.team)
 ]
