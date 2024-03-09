e18d53651c1db319201976c8df1d5bf0d42d0195
Ivan <ivanu1706@gmail.com>
2024-03-09 21:10:37 +1100

    Добавил в ДБ таблицу User

--- Web/welcome/models.py
+++ Web/welcome/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    nickname = models.CharField(blank=False, max_length=75, null=False)
+    login = models.CharField(blank=False, max_length=100, unique=True, null=False)
+    password = models.CharField(blank=False, max_length=50, null=False)
+
+
+    def __str__(self):
+        return f'{self.login}'
+
