f66f9092597fc674a874cacf6bc8b9ae77583f3e
XRenso <tch.xr@yandex.ru>
2024-03-15 15:22:07 +1100

    мега жизнь

--- Web/main/views.py
+++ Web/main/views.py
@@ -6,6 +6,8 @@ from django.views.generic import FormView
 from django.views.generic.edit import CreateView
 from main.models import User
 from main.forms import NewUserForm
+from django.contrib.auth import logout
+
 
 class RegisterView(FormView):
 
@@ -16,43 +18,19 @@ class RegisterView(FormView):
         form.save()
         return super().form_valid(form)
 
-def check_login(cookie):
-    if cookie and 'login' in cookie:
-        return db.get_user(cookie['login'])
-    return None
+def Logout_view(request):
+    logout(request)
+    return redirect('/')
+
 
 def index(request):
-    user = check_login(request.COOKIES)
-    if user:
-        return render(request,'base.html', {'user_login':user['nickname']})
-    else:
-         return render(request,'base.html')
+    return render(request,'base.html')
 
 def team(request):
-    user = check_login(request.COOKIES)
-    if user:
+   return render(request,'team.html')
 
-        return render(request,'team.html', {'user_login':user['nickname']})
-    else:
-        return render(request,'team.html')
-
-def create_cookie(request):
-    data = {'login':'test','nickname':'adolf','password':'mega_gay'}
-    try:
-        db.delete_user(data['login'])
-    except:
-        pass
-    db.add_user(data)
-
-    response = redirect('/')
-    response.set_cookie('login',data['login'])
-    return response
 
 
 def NotFound(request, exception=None):
-    user = check_login(request.COOKIES)
-    if user:
-        return render(request,'404.html',{'user_login':user['nickname']},status=404)
-    else:
-        return render(request,'404.html',status=404)
+    return render(request,'404.html',status=404)
 
